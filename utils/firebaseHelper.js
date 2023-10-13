3b8e8615648ca214409c1bb2cebaebc97231f98a
dongmin30 <dongmin0355@gmail.com>
2023-10-13 17:20:45 +0900

    first commit

---
+++ utils/firebaseHelper.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+export const getFirebaseApp = () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyAWJm7W_AeMgsW3zwwzfaHWwvlpb8q4EgI",
+        authDomain: "capstone-chat-app-1a521.firebaseapp.com",
+        databaseURL: "https://capstone-chat-app-1a521-default-rtdb.firebaseio.com",
+        projectId: "capstone-chat-app-1a521",
+        storageBucket: "capstone-chat-app-1a521.appspot.com",
+        messagingSenderId: "396227281235",
+        appId: "1:396227281235:web:845d0a30afca8023f621a6",
+        measurementId: "G-VNKF2TQFK0"
+    }
+
+    // Initialize Firebase
+    return initializeApp(firebaseConfig)
+}
