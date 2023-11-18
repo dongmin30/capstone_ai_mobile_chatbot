c6f55fbccd84477c0dad17893e3b35fcd282baee
dongmin30 <dongmin0355@gmail.com>
2023-11-18 14:30:46 +0900

    ValidationConstraints 문구 재수정

--- utils/ValidationConstraints.js
+++ utils/ValidationConstraints.js
@@ -24,17 +24,17 @@ export const validateEmail = (id, value) => {
     const constraints = {
         presence: {
             allowEmpty: false,
-            message: '은 비어 있는 값을 넣을 수 없습니다.',
+            message: '이메일은 비어 있는 값을 넣을 수 없습니다.',
         },
     }
 
     if (value !== '') {
         constraints.email = {
-            message: '형식이 올바르지 않습니다.'
+            message: '이메일 형식이 올바르지 않습니다.'
         }
     }
 
-    const validationResult = validate({ [id]: value }, { [id]: constraints })
+    const validationResult = validate({ [id]: constraints })
     return validationResult && validationResult[id]
 }
 
@@ -42,17 +42,17 @@ export const validatePassword = (id, value) => {
     const constraints = {
         presence: {
             allowEmpty: false,
-            message: '는 비어 있는 값을 넣을 수 없습니다.',
+            message: '비밀번호는 비어 있는 값을 넣을 수 없습니다.',
         },
     }
 
     if (value !== '') {
         constraints.length = {
             minimum: 6,
-            message: '는 6자 이상이여야합니다.',
+            message: '비밀번호는 6자 이상이여야합니다.',
         }
     }
 
-    const validationResult = validate({ [id]: value }, { [id]: constraints })
+    const validationResult = validate({ [id]: constraints })
     return validationResult && validationResult[id]
 }
